582aeb31dd54f29bfc7de3b646dbf88ec1b358ed
aariyasreeskumar123 <ariyasree.it.sxcce@gmail.com>
2025-12-09 16:27:06 +0530

    Modified

--- max_and_min.py
+++ max_and_min.py
@@ -5,6 +5,55 @@ def max_and_min(list):
     print("Maximum of List:",max(list))
     print("Minimum of list:",min(list))
 max_and_min([10, 3, 6, 2])
+max_and_min([-10,-3,-6,-2])
 
 #Time Complexity --> O(n)
 #Space Complexity --> O(1)
+'''-------------------------------------------------------------------------'''
+#Find max & min WITHOUT using max(), min() but using sorting
+
+def maxMinBySort(lst):
+    lst.sort()
+    print("\nMinimum Element:",lst[0])
+    print("Maximum Element:",lst[-1])
+maxMinBySort([10,-2,15,100])
+
+'''------------------------------------------------------------------------'''
+def rangeOfList(lst):
+    print("\nRange of List:",max(lst)-min(lst))
+rangeOfList([10, 3, 6, 2])
+
+'''------------------------------------------------------------------------'''
+
+#Find max & min WITHOUT using max(), min(), or sorting
+
+def maxAndmin(lst):
+    max=0
+    min=0
+    for i in range(0,len(lst)):
+        if lst[i]>max:
+            max=lst[i]
+            min=max
+        if lst[i]<min:
+            min=lst[i]
+    print("\nMaximum Element=",max)
+    print("Minimum Element:",min)
+maxAndmin([10,55,2,33])
+maxAndmin([-11,5,-1])  # max=5 and min=-1 but min=-11 hence break logic 
+
+#Above code fails in list of negative numbers
+#So corrected Code
+
+def maxAndmin2(lst):
+    max=lst[0]
+    min=lst[0]
+    for i in range(0,len(lst)):
+        if lst[i]>max:
+            max=lst[i]
+        if lst[i]<min:
+            min=lst[i]
+    print("\nMaximum Element=",max)
+    print("Minimum Element:",min)
+maxAndmin2([10,-55,2,33])
+maxAndmin2([-10,-55,-2,-33])
+
--- sum_of_elements.py
+++ sum_of_elements.py
@@ -10,3 +10,42 @@ sum_of_elements([1,2,3,4])
 
 #Time Complexity --> O(n)
 #Space Complexity --> O(1)
+'''--------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------'''
+#Sum Without Using sum() or '+' Operator
+'''
+Approach: Can solve by subtraction or bitwise operation
+-------------------------------------------------------
+XOR → sum without carry
+
+AND + << 1 → carry
+
+Repeat until carry = 0
+---------------------------
+Solution: 
+---------
+Step 1: XOR OPERATION
+
+        5 - 101             
+        4=  100            
+        ----------
+            001 ---> 1       
+        ----------
+
+Step 2: AND Operation
+            101                                   
+            110
+            ----
+            100 <<1 = 1000 = 8 [Left Shift]
+                     
+Step 3:     1+8=9 
+
+'''
+
+def sum_method(num1,num2):
+    while num2 != 0:
+        carry = (num1 & num2) << 1 # 8 
+        num1 = num1 ^ num2 #num1=1
+        num2 = carry  #num2=8
+    return num1
+print(sum_method(5,4))
